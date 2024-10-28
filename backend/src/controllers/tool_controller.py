410e1cb61842de3672f0b38dd28dfb5e9cb39d3c
BGW-10 <barry.walker1210@gmail.com>
2024-10-28 13:55:24 -0400

    Sending new tool data to the backend

--- backend/src/controllers/tool_controller.py
+++ backend/src/controllers/tool_controller.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 tools = Blueprint("tools", __name__)
 
 @tools.route("/hello/<int:thenumber>", methods = ["GET"])
 def test(thenumber):
     return f"tool route test {thenumber}"
+
+INPUTFIELDS = [
+    ['name', 'size', 'availability', 'image']
+]
+
+@tools.route('/create', methods=['POST'])
+def create_tool():
+    # Create a dictionary to store the data
+    tool_data = {}
+    
+    # Read text fields
+    tool_data['name'] = request.form.get('name')
+    tool_data['size'] = request.form.get('size')
+    tool_data['availability'] = request.form.get('availability')
+    if 'image' in request.files:
+        image = request.files['image']
+    
+    print(image)
+    
+    # Log the tool data for debugging
+    print("Received tool data:", tool_data)
+
+    # Respond with the received data
+    return tool_data, 201
--- frontend/src/components/ToolCatalog.jsx
+++ frontend/src/components/ToolCatalog.jsx
@@ -1,10 +1,17 @@
-import ToolDesc from "./ToolDesc";
+import { useState } from "react";
 
+import ToolDesc from "./ToolDesc";
+import ToolCreator from "./ToolCreator"
 import "./ToolCatalog.css";
+;
 
 function ToolCatalog() {
+    const [creating, setCreating] = useState(false);
+
     return (
         <div className="tool-catalog">
+            <button className="create-tool-button" onClick={() => setCreating(true)}>+</button>
+            <ToolCreator isVisible={creating} onClose={() => setCreating(false)}></ToolCreator>
             <ToolDesc></ToolDesc>
             <ToolDesc></ToolDesc>
             <ToolDesc></ToolDesc>
